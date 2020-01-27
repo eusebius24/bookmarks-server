4c97b06fa2af64df7b22fd4b43857f2ded02a7e4
Erika Reiman <erika.reiman@gmail.com>
2020-01-27 15:52:20 -0500

    Finished assignment

---
+++ src/store.js
@@ -0,0 +1,11 @@
+const bookmarks = [
+    {
+      id: 1,
+      title: 'Google',
+      url: 'https://www.google.com',
+      description: 'The website that knows everything',
+      rating: 3
+    }
+  ]
+
+module.exports = { bookmarks }
